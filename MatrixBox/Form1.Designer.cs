9dd8d0eb438ac9a83926fa5fb4f8a41b673dd4b1
quickbasic22 <quickbasic44@outlook.com>
2021-10-19 15:13:12 -0400

    Add project files.

---
+++ MatrixBox/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace MatrixBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(189, 3);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(10);
+            this.groupBox1.Size = new System.Drawing.Size(589, 720);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(116, 67);
+            this.label1.Margin = new System.Windows.Forms.Padding(10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(369, 333);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hello World and Hello World and Hello World and Hello World and  Hello World and " +
+    "Hello World and Hello World and Hello World and ";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(46, 190);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(76, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 274);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(953, 633);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MatrixBox/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace MatrixBox
+{
+    public partial class Form1 : Form
+    {
+        string displayString = "This is a very long string to measure how much space is needed";
+        Font font8 = new Font("Arial", 8);
+        Font font12 = new Font("Arial", 12);
+        Font font24 = new Font("Arial", 24);
+        Font font36 = new Font("Arial", 36);
+        int fontVar = 0;
+        SizeF sizeF;
+        public Graphics e;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            e = label1.CreateGraphics();
+            
+            numericUpDown1.Minimum = 0;
+            numericUpDown1.Maximum = 3;
+        }
+
+        
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs evt)
+        {
+            
+            fontVar = Int32.Parse(numericUpDown1.Value.ToString());
+            switch (fontVar)
+            {
+                case 0:
+                    label2.Text = "Arial 8";
+                    label3.Text = e.MeasureString(displayString, font8).ToString();
+                    label1.Font = font8;
+                    break;
+                case 1:
+                    label2.Text = "Arial 12";
+                   label3.Text = e.MeasureString(displayString, font12).ToString();
+                    label1.Font = font12;
+                    break;
+                case 2:
+                    label2.Text = "Arial 24";
+                    label3.Text = e.MeasureString(displayString, font24).ToString();
+                    label1.Font = font24;
+                    break;
+                case 3:
+                    label2.Text = "Arial 36";
+                    label3.Text = e.MeasureString(displayString, font36).ToString();
+                    label1.Font = font36;
+                    break;
+            }
+        }
+
+        
+    }
+}
